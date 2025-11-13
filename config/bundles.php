4058735e6518847adf50d20277fdd2f9f19f4f9f
Martina Schulz <tracyolivia@msn.com>
2025-11-13 12:33:08 +0100

    Update dockerfile

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+
 ];
